011bb08c4c6ec3eb2313c64004898c8a232ed593
jbush7401 <jbush6@gmail.com>
2023-11-19 06:30:08 -0700

    Day 8 Complete
    Day 8 done. 11 MS total time

--- AoCCPP/2022/Day8.h
+++ AoCCPP/2022/Day8.h
@@ -13,4 +13,16 @@ private:
     std::vector<std::vector<int>> grid;
 
     bool CheckTreeVisibility(int x, int y);
+    bool CheckUp(int x, int y);
+    bool CheckDown(int x, int y);
+    bool CheckLeft(int x, int y);
+    bool CheckRight(int x, int y);
+
+    int CountTreeVisibility(int x, int y);
+    int CountUp(int x, int y);
+    int CountDown(int x, int y);
+    int CountLeft(int x, int y);
+    int CountRight(int x, int y);
+
+
 };
