a859e11ee91b991de557f069249c9f5b29a8f3f5
jbush7401 <jbush6@gmail.com>
2023-11-29 06:11:35 -0700

    Day 10 template

---
+++ AoCCPP/2022/Day10.cpp
@@ -0,0 +1,14 @@
+#include "Day10.h"
+
+void Day10_2022::PartOne()
+{
+	//Start cycle timer loop until end of commands
+	//Check if ready to execute next command
+	//if 20, 60, 100, 140, 180, or 220 cycle, add to total signal strength
+	//Check if current command is completed and if so add to x.
+	
+}
+
+void Day10_2022::PartTwo()
+{
+}
---
+++ AoCCPP/2022/Day10.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "IDay.h"
+#include <string>
+#include <vector>
+
+class Day10_2022 : public IDay {
+public:
+    void PartOne();
+    void PartTwo();
+};
