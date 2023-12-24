31b13932ec8122fc5b906bde916fee8fd10c36e2
jbush7401 <jbush6@gmail.com>
2023-12-24 10:57:53 -0700

    2023 Day 7 Part 1 Complete
    I need to change some of the original functions for part 2, so pushing what I have so far that works for part 1.

--- AoCCPP/2023/Day7.cpp
+++ AoCCPP/2023/Day7.cpp
@@ -1,12 +1,66 @@
 #include "Day7.h"
 #include <iostream>
 #include "Util.h"
-
+#include <algorithm>
+#include <list>
 
 void Day7_2023::PartOne() {
+	vec = GetVectorFromTxt("2023/Input/Day7.txt");
+	const std::string cards = { '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A' };
 
+	for (std::string s : vec) {
+		Hand h;
+		h.originalHand = s.substr(0, 5);
+
+		h.rank = std::stoi(s.substr(6));
+
+		hands.push_back(h);
+	}
+
+	for (int i = 0; i < hands.size(); i++) {
+		//Find Hand Rank
+		hands[i].handStr = FindRank(hands[i]);
+	}
+
+	std::sort(hands.begin(), hands.end(), [&cards](const Hand& h1, const Hand& h2) {
+		return Hand::CompareHands(h1, h2, cards);
+		});
+	long result = 0;
+	for (int i = 1; i <= hands.size(); i++)
+		result += i * hands[i - 1].rank;
+	std::cout << "Part 1: " << result << std::endl;
 }
 
 void Day7_2023::PartTwo() {
 }
+
+Day7_2023::HandRank Day7_2023::FindRank(Hand& h)
+{
+	std::vector<int> solve;
+	std::string charsDone = "";
+	for (char c : h.originalHand) {
+		if (charsDone.find(c) != std::string::npos)
+			continue;
+		solve.push_back(std::count(h.originalHand.begin(), h.originalHand.end(), c));
+		charsDone.push_back(c);
+	}
+	std::sort(solve.rbegin(), solve.rend());
+	if (solve.size() == 1)
+		return FiveOfAKind;
+	else if (solve[0] == 4)
+		return FourOfAKind;
+	else if (solve[0] == 3 && solve[1] == 2)
+		return FullHouse;
+	else if (solve[0] == 3)
+		return ThreeOfAKind;
+	else if (solve[0] == 2 && solve[1] == 2)
+		return TwoPair;
+	else if (solve[0] == 2)
+		return OnePair;
+	else
+		return HighCard;
+}
+
+
+
 
--- AoCCPP/2023/Day7.h
+++ AoCCPP/2023/Day7.h
@@ -2,19 +2,51 @@
 #include "IDay.h"
 #include <vector>
 #include <string>
+#include <unordered_set>
 
 class Day7_2023 : public IDay {
 public:
     void PartOne(); // declaration of PartOne function
     void PartTwo(); // declaration of PartTwo function
-private:
-    std::vector<std::string> vec;
 
-    enum Card {A, F2, F3, F4, F5, F6, F7, F8, F9, J, Q, K };
+    std::vector<std::string> vec;
+    
+    const enum HandRank { HighCard, OnePair, TwoPair, ThreeOfAKind, FullHouse, FourOfAKind, FiveOfAKind };
 
     class Hand {
+    public:
         std::string originalHand = "";
-        Card hand[5];
+     
+        HandRank handStr = HighCard;
+        int rank = 0;
+
+        static bool CompareHands(const Day7_2023::Hand& h1, const Day7_2023::Hand& h2, const std::string& cards)
+        {
+           
+            if (h1.handStr == h2.handStr) {
+                for (int i = 0; i < 5; i++) {
+                    int h1Rank = cards.find_first_of(h1.originalHand[i]);
+                    int h2Rank = cards.find_first_of(h2.originalHand[i]);
+                    if (h1Rank == h2Rank)
+                        continue;
+                    return h1Rank < h2Rank;
+                }
+            }
+            return h1.handStr < h2.handStr;
+        }
     };
+
+    HandRank FindRank(Hand& h);
+    std::vector<Hand> hands = {};
+
+private:
+    
+    
+
+   
+
+   
+
+    
 
 };
