c6ddc83fdd08ca77a5cb5dd518dba25dc047617d
jbush7401 <jbush6@gmail.com>
2023-12-31 08:17:03 -0700

    Day 8 Part 1 Complete
    140ms or so.

--- AoCCPP/2023/Day8.cpp
+++ AoCCPP/2023/Day8.cpp
@@ -5,6 +5,31 @@
 void Day8_2023::PartOne() {
 	vec = GetVectorFromTxt("2023/Input/Day8.txt");
 
+	instructions = vec[0];
+	Node current;
+	for (int i = 2; i < vec.size(); i++) {
+		dict.insert({ vec[i].substr(0, 3) ,  Node(vec[i].substr(vec[i].find_first_of('(') + 1, 3), vec[i].substr(vec[i].find_first_of(',') + 2, 3)) });
+	}
+	int steps = 0;
 
+	current = dict.at("AAA");
+	bool found = false;
+	while(!found){
+		for (char c : instructions) {
+			std::string next = "";
+			if (c == 'R')
+				next = current.right;
+			else
+				next = current.left;
+			steps++;
+			if (next == "ZZZ") {
+				found = true;
+				break;
+			}
+			current = dict.at(next);
+		}
+	}
+
+	std::cout << "Part 1: " << steps << std::endl;
 }
 void Day8_2023::PartTwo() {}
--- AoCCPP/2023/Day8.h
+++ AoCCPP/2023/Day8.h
@@ -2,6 +2,7 @@
 #include "IDay.h"
 #include <vector>
 #include <string>
+#include <unordered_map>
 
 class Day8_2023 : public IDay {
 public:
@@ -9,4 +10,16 @@ public:
     void PartTwo(); // declaration of PartTwo function
 
     std::vector<std::string> vec;
+
+    std::string instructions;
+
+    struct Node {
+        Node(std::string l = "", std::string r = "") : left(l), right(r) {};
+        std::string left;
+        std::string right;
+    };
+
+    std::unordered_map<std::string, Node> dict;
+
+    
 };
