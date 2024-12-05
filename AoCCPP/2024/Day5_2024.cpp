d06b7262f372e18bb9768286fb75710aa0c0b192
jbush7401 <jbush6@gmail.com>
2024-12-05 15:44:37 -0700

    From 19000+ swaps to 0 swaps. 28ms

--- AoCCPP/2024/Day5_2024.cpp
+++ AoCCPP/2024/Day5_2024.cpp
@@ -4,6 +4,7 @@
 #include <fstream>
 #include <iostream>
 #include <numeric>
+#include <algorithm>
 
 void Day5_2024::PartOne()
 {
@@ -55,29 +56,63 @@ void Day5_2024::PartOne()
 
 }
 
+//void Day5_2024::PartTwo()
+//{
+//    int swap = 0;
+//    // Iterate through the bad updates
+//    for (Update& u : updates) {
+//        if (!u.isValid)
+//        {
+//            bool fixed = false;
+//            while (!fixed)
+//            {
+//                fixed = true;
+//                // Iterate through each number in the update values
+//                for (int v = 0; v < u.values.size(); v++) {
+//                    // Get the protocol for the current update value
+//                    std::vector<int> prot = protocols[u.values[v]];
+//                    // Are any of the values below the current value in the update in the protocol?
+//                    for (int c = 0; c < v; c++)
+//                        if (std::find(prot.begin(), prot.end(), u.values[c]) != prot.end()) {
+//                            //Move values[v] to index c
+//                            MoveElement(u.values, v, c);
+//                            swap++;
+//                            fixed = false;
+//                        }
+//                }
+//            }
+//        }
+//    }
+//    int sum = 0;
+//
+//    for (Update i : updates) {
+//        sum += i.isValid ? 0 : i.values[i.values.size() / 2];
+//    }
+//    std::cout << "Part 2: " << sum << " Swapped: " << swap << std::endl;
+//
+//}
+
 void Day5_2024::PartTwo()
 {
+    int swap = 0;
     // Iterate through the bad updates
     for (Update& u : updates) {
         if (!u.isValid)
         {
-            bool fixed = false;
-            while (!fixed)
-            {
-                fixed = true;
-                // Iterate through each number in the update values
-                for (int v = 0; v < u.values.size(); v++) {
-                    // Get the protocol for the current update value
-                    std::vector<int> prot = protocols[u.values[v]];
-                    // Are any of the values below the current value in the update in the protocol?
-                    for (int c = 0; c < v; c++)
-                        if (std::find(prot.begin(), prot.end(), u.values[c]) != prot.end()) {
-                            //Move values[v] to index c
-                            MoveElement(u.values, v, c);
-                            fixed = false;
-                        }
-                }
+            std::sort(u.values.begin(), u.values.end());
+            std::vector<int> newOrder = u.values;
+
+            // Iterate through this updates values, check each prop how many of the other update values should be in front of this one.
+            for (int i = 0; i < u.values.size(); i++) {
+                int val = u.values[i];
+                std::vector<int> prot = protocols[val]; 
+                std::vector<int> common;
+                std::sort(prot.begin(), prot.end());
+                std::set_intersection(u.values.begin(), u.values.end(), prot.begin(), prot.end(), std::back_inserter(common));
+                int count = common.size();
+                newOrder[newOrder.size() - count - 1] = val;
             }
+            u.values = newOrder;
         }
     }
     int sum = 0;
@@ -85,7 +120,7 @@ void Day5_2024::PartTwo()
     for (Update i : updates) {
         sum += i.isValid ? 0 : i.values[i.values.size() / 2];
     }
-    std::cout << "Part 2: " << sum << std::endl;
+    std::cout << "Part 2: " << sum << " Swapped: " << swap << std::endl;
 
 }
 
